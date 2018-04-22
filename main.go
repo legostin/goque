c466440917cfd63d5de254ff61e5c439d96521c5
legostin <84d83ca6>
2018-04-22 17:54:15 +0600

    added GET and simple POST requests support

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 
 func NewRedisClient() *redis.Client {
 	client := redis.NewClient(&redis.Options{
-		Addr:     "127.0.0.1:6379",
+		Addr:     "redis:6379",
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
