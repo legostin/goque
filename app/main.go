64117cdd27111f7e286dac3562c9d49f24277c10
legostin <84d83ca6>
2018-04-22 11:32:47 +0600

    added routing

--- app/main.go
+++ app/main.go
@@ -11,8 +11,8 @@ func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", Hello)
 	http.Handle("/", r)
-	fmt.Println("Starting up on 8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	fmt.Println("Starting up on 8020")
+	log.Fatal(http.ListenAndServe(":8020", nil))
 }
 
 func Hello(w http.ResponseWriter, req *http.Request) {
